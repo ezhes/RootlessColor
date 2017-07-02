e64f992720b01b78fb6528e234eb6b3d70f4d70a
Allison Husain <allison.husain@berkeley.edu>
2017-07-02 11:11:40 -0600

    Initial and likely only

---
+++ RootlessColor/GammaController.h
@@ -0,0 +1,18 @@
+//
+//  GammaController.h
+//  GammaTest
+//
+//  Created by Thomas Finch on 6/22/15.
+//  Copyright (c) 2015 Thomas Finch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GammaController : NSObject
+
++ (void)setGammaWithRed:(float)red green:(float)green blue:(float)blue;
++ (void)setGammaWithOrangeness:(float)percentOrange;
++ (void)setEnabled:(BOOL)enabled;
++ (void)autoChangeOrangenessIfNeeded;
+
+@end
