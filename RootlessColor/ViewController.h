eaa84bc387e6051d34065bd6052bbf6ad320dabf
Allison Husain <allison.husain@berkeley.edu>
2017-07-02 10:00:09 -0600

    Initial Commit

---
+++ RootlessColor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RootlessColor
+//
+//  Created by Salman Husain on 7/2/17.
+//  Copyright © 2017 Salman Husain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RootlessColor/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RootlessColor
+//
+//  Created by Salman Husain on 7/2/17.
+//  Copyright © 2017 Salman Husain. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
