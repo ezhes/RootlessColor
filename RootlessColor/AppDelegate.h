58bbedebc562aac0b771b5746b7148d90fc4bf61
Github <shusain93@gmail.com>
2017-07-02 10:00:09 -0600

    Initial Commit

---
+++ RootlessColor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RootlessColor
+//
+//  Created by Salman Husain on 7/2/17.
+//  Copyright © 2017 Salman Husain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
